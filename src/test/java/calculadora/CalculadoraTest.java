852e9de66da56b4f6b6fc715d5285bb125951453
9900666 - Luis Felipe Da Silva <9900666@prestadorserv.com.br>
2024-07-14 19:00:00 -0300

    Sysout

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -8,21 +8,25 @@ public class CalculadoraTest {
 
     @Test
     public void testSomar() {
+    	System.out.println("Soma: " + calc.somar(5, 3));
         assertEquals(8, calc.somar(5, 3), 0);
     }
 
     @Test
     public void testSubtrair() {
+    	System.out.println("Subtração: " + calc.subtrair(5, 3));
         assertEquals(2, calc.subtrair(5, 3), 0);
     }
 
     @Test
     public void testMultiplicar() {
+    	System.out.println("Multiplicação: " + calc.multiplicar(5, 3));
         assertEquals(15, calc.multiplicar(5, 3), 0);
     }
 
     @Test
     public void testDividir() {
+    	System.out.println("Divisão: " + calc.dividir(5, 3));
         assertEquals(1.6667, calc.dividir(5, 3), 0.0001);
     }
 
